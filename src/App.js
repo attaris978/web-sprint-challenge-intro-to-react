86c3284b7e2f0807b53b22e3fa1a07f1f4c156da
unknown <ryancare@protonmail.com>
2022-03-24 21:04:37 -0500

    Added useEffect fetch request, importing url from constants.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import api_url from "./components/constants";
 
 const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(api_url);
+      const json = await response.json();
+      json.ok ? setData(json.results) : console.error(`fetch error: ${json}`);
+      document.querySelector("p").innerText = json.results[0].name;
+      console.log(json.results);
+    };
+    fetchData().catch((err) => console.error(err));
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+    return ( 
+      
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        <p>Hi</p>
+      </div>
+    ) 
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+const Character = (props) => {
+    return (
+        <p>{props[0].name}</p>
+    )
+}
+
+export default Character;
