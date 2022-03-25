7e876220ceed3c5f37fa366bd2deeaceb6a3c828
unknown <ryancare@protonmail.com>
2022-03-24 21:47:02 -0500

    Added first styled component to color text

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,14 +1,19 @@
 import Character from "./Character";
+import styled from "styled-components";
+
+const CharDiv = styled.div`
+    color:  lemonchiffon;
+    `
 const Characters = (props) => {
     const {characters} = props;
     return (
-        <div>
+        <CharDiv>
             {characters && characters.map(char => {
                 return (
                     <Character name={char.name} key={props.name}/>
                 )
             }) }
-            </div>
+            </CharDiv>
         
 
     )
